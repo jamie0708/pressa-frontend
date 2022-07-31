e50b1728771006bb93b466cb3ec893d6191c549c
jamie0708 <juniorschool20@gmail.com>
2022-07-31 12:37:30 +0500

    socket

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import { React, useEffect }from "react";
 import { Routes, Route } from "react-router-dom";
+import io from "socket.io-client";
 import Auth from "./views/Auth/Auth";
 import SinglePage from "./views/SinglePage/SinglePage";
 import HomePage from "./views/HomePage/HomePage";
 import AboutPage from "./views/AboutPage/AboutPage";
-
 import "./_app.scss";
 import AnnouncePage from "./views/AnnouncePage/AnnouncePage";
 import AdminPage from "./views/AdminPage/AdminPage";
 
+let socket;
+const CONNECTION_PORT = "http://localhost:6699";
+
 function App() {
+
+  useEffect(() => {
+    socket = io(CONNECTION_PORT);
+  }, [CONNECTION_PORT]);
+
   return (
     <div className="app">
       <div className="app__container">
